b6b9c633b0f8dce904df32ad3bfedc13ee1a7e3f
AlbertComb <70507584+AlbertComb@users.noreply.github.com>
2021-05-20 16:06:21 +0200

    Bug fix

--- src/main/java/it/uniroma3/siw/spring/controller/OperaController.java
+++ src/main/java/it/uniroma3/siw/spring/controller/OperaController.java
@@ -1,5 +1,59 @@
 package it.uniroma3.siw.spring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import it.uniroma3.siw.spring.model.Opera;
+import it.uniroma3.siw.spring.service.OperaService;
+
+@Controller
 public class OperaController {
 
+	@Autowired
+	private OperaService operaService;
+	
+    @Autowired
+    private OperaValidator operaValidator;
+    
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+
+    @RequestMapping(value="/addOpera", method = RequestMethod.GET)
+    public String addOpera(Model model) {
+    	logger.debug("addOpera");
+    	model.addAttribute("opera", new Opera());
+        return "personaForm.html";
+    }
+
+    @RequestMapping(value = "/opera/{id}", method = RequestMethod.GET)
+    public String getOpera(@PathVariable("id") Long id, Model model) {
+    	model.addAttribute("opera", this.operaService.operaPerId(id));
+    	return "persona.html";
+    }
+
+    @RequestMapping(value = "/persona", method = RequestMethod.GET)
+    public String getOpere(Model model) {
+    		model.addAttribute("opere", this.operaService.tutte());
+    		return "persone.html";
+    }
+    
+    @RequestMapping(value = "/persona", method = RequestMethod.POST)
+    public String newOpera(@ModelAttribute("opera") Opera opera, 
+    									Model model, BindingResult bindingResult) {
+    	this.operaValidator.validate(opera, bindingResult);
+        if (!bindingResult.hasErrors()) {
+        	this.operaService.inserisci(opera);
+            model.addAttribute("persone", this.operaService.tutte());
+            return "persone.html";
+        }
+        return "personaForm.html";
+    }
 }
