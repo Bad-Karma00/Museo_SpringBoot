5714efb0518a7743d670ba37d3e4dd1c0b0b9d36
Bad-Karma00 <72249553+Bad-Karma00@users.noreply.github.com>
2021-05-20 12:44:25 +0200

    Modifiche al pom

--- src/main/java/it/uniroma3/siw/spring/model/Collezione.java
+++ src/main/java/it/uniroma3/siw/spring/model/Collezione.java
@@ -3,6 +3,7 @@ package it.uniroma3.siw.spring.model;
 import java.util.List;
 
 
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,8 +15,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotEmpty;
-
-import it.uniroma3.progetto.model.Autore;
 
 
 
