46f60f4f25c90c166761519b72ff19520d962f02
Bad-Karma00 <72249553+Bad-Karma00@users.noreply.github.com>
2021-06-04 12:38:36 +0200

    Cambiamenti all'index per avere l'href sul nome dell'opera

--- src/main/java/it/uniroma3/siw/spring/controller/AuthenticationController.java
+++ src/main/java/it/uniroma3/siw/spring/controller/AuthenticationController.java
@@ -1,11 +1,9 @@
 package it.uniroma3.siw.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
