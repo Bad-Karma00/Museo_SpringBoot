12694b039283cd83a1d5423f584787a741f9f6cf
Bad-Karma00 <72249553+Bad-Karma00@users.noreply.github.com>
2021-06-05 15:41:08 +0200

    Aggiunta possibilità di ordinare artisti e opere

--- src/main/java/it/uniroma3/siw/spring/model/Credentials.java
+++ src/main/java/it/uniroma3/siw/spring/model/Credentials.java
@@ -8,8 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "credentials")
 public class Credentials {
 	
 	
